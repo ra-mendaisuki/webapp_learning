d27cde522623f83f66fc947ca29227f1b670c3d1
ra-mendaisuki <roaroa18@gmail.com>
2026-05-21 23:33:06 +0900

    繰り返し分ありのテンプレート作成

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template
 
 app: Flask = Flask(__name__)
 
+class Message:
+    def __init__(self, id:str, user_name:str, contents: str):
+        self.id = id
+        self.user_name = user_name
+        self.contents = contents
 
 # 「/」にアクセスがあった場合のルーティング
 @app.route('/')
 def index():
     login_user_name: str = "osamu"
-    return render_template("top.html", login_user_name=login_user_name)
+
+    message_list = [
+        Message("202400502102310", "osamu", "朝からビールですか？楽しみですね"),
+        Message("202400502102311", "noriko", "こちらこそ！次回はABコースです！"),
+        Message("202400502102312", "osamu", "昨日はHBコース楽しかったです。")
+    ]
+    return render_template(
+        "top.html", login_user_name=login_user_name, message_list=message_list
+    )
 
 
 # 「/write」でアクセスがあった場合のルーティング
