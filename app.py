862c75ac49e0b6a29726c6457467ce4e1caa489f
ra-mendaisuki <roaroa18@gmail.com>
2026-05-21 21:47:33 +0900

    意図したエラー箇所を修正

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    age: int = 19
-    return "<h1>あなたの年齢は" + age + "歳です</h1>"
+    return "<h1>あなたの年齢は20歳です</h1>"
 
 
 if __name__ == '__main__':
