135ef9e0f63bf8960c799c07d0b92fce2c421825
ra-mendaisuki <roaroa18@gmail.com>
2026-05-21 23:04:09 +0900

    同じルーティングでメソッド設定を変更したものを設定

--- app.py
+++ app.py
@@ -9,10 +9,24 @@ def index():
     return "<h1> これは掲示板のトップページです </h1>"
 
 
-# 「/write」２アクセスが合った場合のルーティング
-@app.route("/write")
-def write():
-    return "<h1> これは掲示板の書き込みページです。 </h1>"
+# 「/write」にGETメソッドでアクセスがあった場合のルーティング
+@app.route("/write", methods=['GET'])
+def write_by_get_method():
+    return """
+    <html><body>
+    <h1>これは掲示板の書き込みページです。</h1>
+    <h3>書き込み内容</h3>
+    <form action="/write" method="post">
+        <textarea name="msg" rows="5" cols="70"></textarea><br/><br/>
+        <input type="submit" value="書き込み">
+    </form>
+    </body></html>
+    """
+
+# 「/write」にPOSTメソッドでアクセスがあった場合のルーティング
+@app.route("/write", methods=['POST'])
+def write_by_post_method():
+    return "<h1>書き込みを受け付けました</h1>"
 
 # 「/edit/message_id」にアクセスが合った場合のルーティング
 @app.route("/edit/<int:message_id>")
