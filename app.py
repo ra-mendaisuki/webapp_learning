ae785eb617e29ff5294368ea10c8e40596d64b08
ra-mendaisuki <roaroa18@gmail.com>
2026-05-21 21:52:00 +0900

    複数のルーティングを設定

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 
+# 「/」にアクセスがあった場合のルーティング
 @app.route('/')
-def hello_world():  # put application's code here
-    return "<h1>あなたの年齢は20歳です</h1>"
+def index():
+    return "<h1> これは掲示板のトップページです </h1>"
 
 
-if __name__ == '__main__':
+# 「/write」２アクセスが合った場合のルーティング
+@app.route("/write")
+def write():
+    return "<h1> これは掲示板の書き込みページです。 </h1>"
+
+# 「/edit/message_id」にアクセスが合った場合のルーティング
+@app.route("/edit/<int:message_id>")
+def edit(message_id):
+    return f"<h1>これはID={type(message_id).__name__}の編集ページです。</h1>"
+
+if __name__ == "__main__":
     app.run(debug=True)
