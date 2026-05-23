9bb45b4a5322fac4c1cf41c6edc87da95347bdbf
ra-mendaisuki <roaroa18@gmail.com>
2026-05-23 11:31:46 +0900

    SQLiteを実装

--- app.py
+++ app.py
@@ -1,21 +1,18 @@
 from datetime import datetime
-
-from flask import Flask, render_template, request
-
-class Message:
-    def __init__(self, id:str, user_name:str, contents: str):
-        self.id = id
-        self.user_name = user_name
-        self.contents = contents
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 # グローバル変数の宣言
 app: Flask = Flask(__name__)
 login_user_name:str = "osamu"
-message_list: list[Message] = [
-    Message("202400502102310", "osamu", "朝からビールですか？楽しみですね"),
-    Message("202400502102311", "noriko", "こちらこそ！次回はABコースです！"),
-    Message("202400502102312", "osamu", "昨日はHBコース楽しかったです。")
-]
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
+db = SQLAlchemy(app)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(100))
+    contents = db.Column(db.String(100))
 
 # 「/」にアクセスがあった場合のルーティング
 @app.route('/')
@@ -25,23 +22,15 @@ def index():
 
     # search_word変数の有無を判定
     if search_word is None:
-        # すべてのメッセージを「top.html」に表示
-        return render_template(
-            "top.html",
-            login_user_name=login_user_name,
-            message_list=message_list
-        )
+        message_list: list[Message] = Message.query.all()
     else:
-        # 検索ワードでフィルターしたメッセージを「top.html」に表示
-        filtered_message_list: list[Message] = [
-            message for message in message_list if search_word in message.contents
-        ]
-        return render_template(
-            "top.html",
-            login_user_name=login_user_name,
-            message_list=filtered_message_list,
-            search_word=search_word
-        )
+        message_list: list[Message] = Message.query.filter(Message.contents.like(f"%{search_word}%")).all()
+    return render_template(
+        "top.html",
+        login_user_name=login_user_name,
+        message_list=message_list,
+        search_word=search_word
+    )
 
 # 「/write」でアクセスがあった場合のルーティング
 @app.route("/write", methods=["POST", "GET"])
@@ -52,20 +41,38 @@ def write():
 
     # POST method
     elif request.method == "POST":
-        id: str = datetime.now().strftime("%Y%m%d%H%M%S")
         contents: str = request.form.get("contents")
         user_name: str = request.form.get("user_name")
-        if contents:
-            message_list.insert(0, Message(id, user_name, contents))
-        return render_template(
-            "write.html",
-            login_user_name=login_user_name,
-            message_list=message_list
-        )
-# 「/edit/message_id」にアクセスが合った場合のルーティング
-@app.route("/edit/<int:message_id>")
-def edit(message_id):
-    return f"<h1>これはID={type(message_id).__name__}の編集ページです。</h1>"
+        new_message: Message = Message(user_name=user_name, contents=contents)
+        db.session.add(new_message)
+
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+@app.route("/update/<int:message_id>", methods=["POST", "GET"])
+def update(message_id:int):
+    message: Message = Message.query.get(message_id)
+    if request.method == "GET":
+        return render_template("update.html", login_user_name=login_user_name, message=message)
+
+    elif request.method == "POST":
+        message.contents = request.form.get("contents")
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+@app.route("/delete/<int:message_id>")
+def delete(message_id: int):
+    message: Message = Message.query.get(message_id)
+    db.session.delete(message)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+# データベースの初期化
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
