fce5c3ba82d9a5b1ee924d51ba89c375dc1e8853
ra-mendaisuki <roaroa18@gmail.com>
2026-05-23 19:02:24 +0900

    ログイン機能を実装しました。

--- app.py
+++ app.py
@@ -2,17 +2,68 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
 # グローバル変数の宣言
 app: Flask = Flask(__name__)
-login_user_name:str = "osamu"
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
 db = SQLAlchemy(app)
+
+# ログインマネージャーの設定
+app.config["SECRET_KEY"] = os.urandom(24)
+login_manager: LoginManager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(25))
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(100))
     contents = db.Column(db.String(100))
+
+@login_manager.user_loader
+def load_user(user_id: int):
+    return User.query.get(user_id)
+
+@app.before_request
+def set_login_user_name():
+    global login_user_name
+    login_user_name = current_user.username if current_user.is_authenticated else None
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username: str = request.form.get("username")
+        password: str = request.form.get("password")
+        user = User(username=username, password=generate_password_hash((password)))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        render_template("login.html")
+
+    elif request.method == "POST":
+        username: str = request.form.get("username")
+        password: str = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/')
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect('/')
 
 # 「/」にアクセスがあった場合のルーティング
 @app.route('/')
@@ -53,6 +104,7 @@ def write():
 @app.route("/update/<int:message_id>", methods=["POST", "GET"])
 def update(message_id:int):
     message: Message = Message.query.get(message_id)
+
     if request.method == "GET":
         return render_template("update.html", login_user_name=login_user_name, message=message)
 
