63a0b37a06981dfcc1f71ec624ce00200404b3f6
ra-mendaisuki <roaroa18@gmail.com>
2026-05-23 10:29:18 +0900

    掲示板アプリの作成

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, render_template
-
-app: Flask = Flask(__name__)
+from flask import Flask, render_template, request
 
 class Message:
     def __init__(self, id:str, user_name:str, contents: str):
@@ -8,23 +6,43 @@ class Message:
         self.user_name = user_name
         self.contents = contents
 
+# グローバル変数の宣言
+app: Flask = Flask(__name__)
+login_user_name:str = "osamu"
+message_list: list[Message] = [
+    Message("202400502102310", "osamu", "朝からビールですか？楽しみですね"),
+    Message("202400502102311", "noriko", "こちらこそ！次回はABコースです！"),
+    Message("202400502102312", "osamu", "昨日はHBコース楽しかったです。")
+]
+
 # 「/」にアクセスがあった場合のルーティング
 @app.route('/')
 def index():
-    login_user_name: str = "osamu"
+    # GETメソッドのフォームの値を取得
+    search_word: str = request.args.get("search_word")
 
-    message_list = [
-        Message("202400502102310", "osamu", "朝からビールですか？楽しみですね"),
-        Message("202400502102311", "noriko", "こちらこそ！次回はABコースです！"),
-        Message("202400502102312", "osamu", "昨日はHBコース楽しかったです。")
-    ]
-    return render_template(
-        "top.html", login_user_name=login_user_name, message_list=message_list
-    )
-
+    # search_word変数の有無を判定
+    if search_word is None:
+        # すべてのメッセージを「top.html」に表示
+        return render_template(
+            "top.html",
+            login_user_name=login_user_name,
+            message_list=message_list
+        )
+    else:
+        # 検索ワードでフィルターしたメッセージを「top.html」に表示
+        filtered_message_list: list[Message] = [
+            message for message in message_list if search_word in message.contents
+        ]
+        return render_template(
+            "top.html",
+            login_user_name=login_user_name,
+            message_list=filtered_message_list,
+            search_word=search_word
+        )
 
 # 「/write」でアクセスがあった場合のルーティング
-@app.route("/write")
+@app.route("/write", methods=["POST", "GET"])
 def write():
     return render_template("write.html")
 
